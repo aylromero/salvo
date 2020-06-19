7aa0f66831ca188e1d35bd6d6bdef4795c7261b3
Aylén A. Romero <aylenazulromero@gmail.com>
2020-06-19 01:03:29 -0300

    Delete ScoreRepository.java

--- src/main/java/com/codeoftheweb/salvo/repositories/ScoreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeoftheweb.salvo.repositories;
-
-import com.codeoftheweb.salvo.entities.Score;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScoreRepository extends JpaRepository<Score, Long> {
-}
