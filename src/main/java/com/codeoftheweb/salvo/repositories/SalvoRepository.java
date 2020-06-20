9f1f8ddcc7957f177c96f0637f5b859ee37e9b78
Aylén A. Romero <aylenazulromero@gmail.com>
2020-06-20 02:09:55 -0300

    Delete SalvoRepository.java

--- src/main/java/com/codeoftheweb/salvo/repositories/SalvoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codeoftheweb.salvo.repositories;
-
-import com.codeoftheweb.salvo.entities.Salvo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SalvoRepository extends JpaRepository<Salvo, Long> {
-
-}
